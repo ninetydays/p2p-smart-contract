85d7bfd00b40c7bcb0140551493c6af0f358ba43
JonghunYu <yuzong@naver.com>
2023-09-05 13:25:21 +0900

    feat: basic implementation

--- contracts/PeerToPeer.sol
+++ contracts/PeerToPeer.sol
@@ -5,30 +5,82 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract PeerToPeer {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    struct Request {
+        uint amount;
+        uint duration;
+        uint payoff;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    struct Loan {
+        uint amount;
+        uint duration;
+        uint payoff;
+        address payable lender;
+        address payable borrower;
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    mapping(address => Request) private requests;
+    mapping(address => Loan) private loans;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    event Requested(
+        address indexed borrower,
+        uint amount,
+        uint duration,
+        uint payoff
+    );
+    event Cancelled(address indexed borrower);
+    event Paid(address indexed lender, address indexed borrower, uint amount);
+    event Repaid(
+        address indexed lender,
+        address indexed borrower,
+        uint amount,
+        uint payoff
+    );
 
-        owner.transfer(address(this).balance);
+    function request(uint amount, uint duration, uint payoff) {
+        require(
+            requests[msg.sender].amount > 0,
+            "You can request only one loan"
+        );
+        require(amount > 0, "amount has to be bigger than 0");
+        require(duration > 0, "duration has to be bigger than 0");
+
+        requests[msg.sender] = Request({
+            amount: amount,
+            duration: duration,
+            payoff: payoff
+        });
+
+        emit Requested(msg.sender, amount, duration, payoff);
+    }
+
+    function cancel() {
+        delete requests[msg.sender];
+        emit Cancelled(msg.sender);
+    }
+
+    function pay(address payable borrower) payable {
+        Request request = requests[borrower];
+        (bool succ, ) = payable(this).call{value: request.amount}("");
+        require(succ, "Failed to send Ether");
+        (bool succ, ) = borrower.call{value: request.amount}("");
+        require(succ, "Failed to send Ether");
+        loans[borrower] = Loan({
+            amount: request.amount,
+            duration: request.duration,
+            payoff: request.payoff,
+            lender: msg.sender,
+            borrower: borrower
+        });
+        emit Paid(msg.sender, borrower, request.amount);
+        delete requests[borrower];
+    }
+
+    function repay() payable {
+        Loan loan = loans[msg.sender];
+        (bool succ, ) = loan.lender.call{value: loan.payoff + loan.amount}("");
+        require(succ, "Failed to send Ether");
+        emit Repaid(loan.lender, msg.sender, loan.amount, loan.payoff);
+        delete loans[msg.sender];
     }
 }
