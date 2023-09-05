9cfd9642cd89ecc2673650bdc612cb02fb63bc13
JonghunYu <yuzong@naver.com>
2023-09-05 13:59:36 +0900

    fix: fixed compile error

--- contracts/PeerToPeer.sol
+++ contracts/PeerToPeer.sol
@@ -4,21 +4,21 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+struct Request {
+    uint amount;
+    uint duration;
+    uint payoff;
+}
+
+struct Loan {
+    uint amount;
+    uint duration;
+    uint payoff;
+    address payable lender;
+    address payable borrower;
+}
+
 contract PeerToPeer {
-    struct Request {
-        uint amount;
-        uint duration;
-        uint payoff;
-    }
-
-    struct Loan {
-        uint amount;
-        uint duration;
-        uint payoff;
-        address payable lender;
-        address payable borrower;
-    }
-
     mapping(address => Request) private requests;
     mapping(address => Loan) private loans;
 
@@ -37,7 +37,7 @@ contract PeerToPeer {
         uint payoff
     );
 
-    function request(uint amount, uint duration, uint payoff) {
+    function request(uint amount, uint duration, uint payoff) public {
         require(
             requests[msg.sender].amount > 0,
             "You can request only one loan"
@@ -54,30 +54,28 @@ contract PeerToPeer {
         emit Requested(msg.sender, amount, duration, payoff);
     }
 
-    function cancel() {
+    function cancel() public {
         delete requests[msg.sender];
         emit Cancelled(msg.sender);
     }
 
-    function pay(address payable borrower) payable {
-        Request request = requests[borrower];
-        (bool succ, ) = payable(this).call{value: request.amount}("");
-        require(succ, "Failed to send Ether");
-        (bool succ, ) = borrower.call{value: request.amount}("");
+    function pay(address payable borrower) public payable {
+        Request memory item = requests[borrower];
+        (bool succ, ) = borrower.call{value: item.amount}("");
         require(succ, "Failed to send Ether");
         loans[borrower] = Loan({
-            amount: request.amount,
-            duration: request.duration,
-            payoff: request.payoff,
-            lender: msg.sender,
+            amount: item.amount,
+            duration: item.duration,
+            payoff: item.payoff,
+            lender: payable(msg.sender),
             borrower: borrower
         });
-        emit Paid(msg.sender, borrower, request.amount);
+        emit Paid(msg.sender, borrower, item.amount);
         delete requests[borrower];
     }
 
-    function repay() payable {
-        Loan loan = loans[msg.sender];
+    function repay() public payable {
+        Loan memory loan = loans[msg.sender];
         (bool succ, ) = loan.lender.call{value: loan.payoff + loan.amount}("");
         require(succ, "Failed to send Ether");
         emit Repaid(loan.lender, msg.sender, loan.amount, loan.payoff);
